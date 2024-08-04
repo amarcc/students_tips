2d096e3b6e5319d3a675f29568220202d475ce88
amarhunter01 <amarhunter01@gmail.com>
2024-08-04 12:44:48 +0200

    fixed deployment

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite('resources/css/app.css')
+    @vite(['resources/js/app.js'])
     <title>Studnets-tips</title>
 </head>
 
